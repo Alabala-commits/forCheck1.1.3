ffcd2f23a3892b05c540a7ea706c23d5caf2c36e
Alabala-commits <66037047+Alabala-commits@users.noreply.github.com>
2021-07-28 14:48:25 +0300

    ready for check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,5 @@
-import dao.UserDao;
-import dao.UserDaoJDBCImpl;
 import model.User;
+import service.UserServiceImpl;
 
 import java.util.List;
 
@@ -8,25 +7,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        UserDao userDao = new UserDaoJDBCImpl();
-        userDao.createUsersTable();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
 
         User user1 = new User("Gordan", "Freeman", (byte) 32);
         User user2 = new User("Bailey", "Whitfield", (byte) 77);
         User user3 = new User("Martin", "Hellman", (byte) 75);
         User user4 = new User("Mikle", "Jackson", (byte) 50);
-        addUsersAndPrintInfo(userDao, user1, user2, user3, user4);
+        addUsersAndPrintInfo(userService, user1, user2, user3, user4);
 
-        List<User> users = userDao.getAllUsers();
+        List<User> users = userService.getAllUsers();
         printUsers(users);
 
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 
-    private static void addUsersAndPrintInfo(UserDao userDao, User ... users) {
+    private static void addUsersAndPrintInfo(UserServiceImpl userService, User ... users) {
         for (User user : users) {
-            userDao.saveUser(user.getName(), user.getLastName(), user.getAge());
+            userService.saveUser(user.getName(), user.getLastName(), user.getAge());
             System.out.println("User с именем – \"" + user.getName() + "\" добавлен в базу данных");
         }
     }
--- src/main/java/dao/UserDaoJDBCImpl.java
+++ src/main/java/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,5 @@
 package dao;
 
-import dao.UserDao;
 import model.User;
 import util.Util;
 
@@ -10,154 +9,118 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private static final String CLEAN_TABLE_SQL  = "TRUNCATE TABLE usersTable";
-    private static final String SELECT_ALL_SQL   = "SELECT * FROM usersTable";
-    private static final String REMOVE_BY_ID_SQL = "DELETE FROM usersTable WHERE id = ?";
-    private static final String SAVE_USER_SQL    = "INSERT INTO usersTable (name, lastName, age) VALUES (?, ?, ?)";
-    private static final String DROP_TABLE_SQL   = "DROP TABLE IF EXISTS usersTable";
-    private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS usersTable " +
-                                                   "(id INT AUTO_INCREMENT PRIMARY KEY, " +
-                                                   " name VARCHAR(30), " +
-                                                   " lastName VARCHAR (30), " +
-                                                   " age INT)";
-
     public UserDaoJDBCImpl() {
 
     }
 
     @Override
     public void createUsersTable() {
-        executionSql(CREATE_TABLE_SQL);
+        executionSql("CREATE TABLE IF NOT EXISTS usersTable " +
+                     "(id INT AUTO_INCREMENT PRIMARY KEY, " +
+                     " name VARCHAR(30), " +
+                     " lastName VARCHAR (30), " +
+                     " age INT)");
     }
 
     @Override
     public void dropUsersTable() {
-        executionSql(DROP_TABLE_SQL);
+        executionSql("DROP TABLE IF EXISTS usersTable");
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Connection        conn  = null;
-        PreparedStatement pstmt = null;
 
-        try {
-            conn = Util.connection();
+        try (Connection conn = Util.connection()) {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             conn.setAutoCommit(false);
 
-            pstmt = conn.prepareStatement(SAVE_USER_SQL);
-            pstmt.setString(1, name);
-            pstmt.setString(2, lastName);
-            pstmt.setByte(3, age);
-            pstmt.executeUpdate();
+            try (PreparedStatement pstmt = conn.prepareStatement(
+                    "INSERT INTO usersTable (name, lastName, age) VALUES (?, ?, ?)")) {
+                pstmt.setString(1, name);
+                pstmt.setString(2, lastName);
+                pstmt.setByte(3, age);
+                pstmt.executeUpdate();
 
-            conn.commit();
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
-            rollback(conn);
             e.printStackTrace();
-        } finally {
-            close(pstmt);
-            close(conn);
         }
-
     }
 
     @Override
     public void removeUserById(long id) {
-        Connection        conn  = null;
-        PreparedStatement pstmt = null;
 
-        try {
-            conn = Util.connection();
+        try (Connection conn = Util.connection()) {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             conn.setAutoCommit(false);
 
-            pstmt = conn.prepareStatement(REMOVE_BY_ID_SQL);
-            pstmt.setLong(1, id);
-            pstmt.executeUpdate();
+            try (PreparedStatement pstmt = conn.prepareStatement(
+                    "DELETE FROM usersTable WHERE id = ?")) {
+                pstmt.setLong(1, id);
+                pstmt.executeUpdate();
 
-            conn.commit();
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
+            }
         } catch (SQLException e) {
-            rollback(conn);
             e.printStackTrace();
-        } finally {
-            close(pstmt);
-            close(conn);
         }
     }
 
     @Override
     public List<User> getAllUsers() {
-        Connection conn   = null;
-        Statement  stmt   = null;
-        ResultSet  rs     = null;
         List<User> result = new ArrayList<>();
 
-        try {
-            conn = Util.connection();
+        try (Connection conn = Util.connection()) {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             conn.setAutoCommit(false);
 
-            stmt = conn.createStatement();
-            rs   = stmt.executeQuery(SELECT_ALL_SQL);
+            try (Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT * FROM usersTable")) {
 
-            while (rs.next()) {
-                User user = new User(rs.getString(2), rs.getString(3), rs.getByte(4));
-                user.setId(rs.getLong(1));
-                result.add(user);
+                while (rs.next()) {
+                    User user = new User(rs.getString(2), rs.getString(3), rs.getByte(4));
+                    user.setId(rs.getLong(1));
+                    result.add(user);
+                }
+                conn.commit();
+            } catch (SQLException e) {
+                conn.rollback();
+                e.printStackTrace();
             }
-            conn.commit();
         } catch (SQLException e) {
-            rollback(conn);
             e.printStackTrace();
-        } finally {
-            close(rs);
-            close(stmt);
-            close(conn);
         }
         return result;
     }
 
     @Override
     public void cleanUsersTable() {
-        executionSql(CLEAN_TABLE_SQL);
+        executionSql("TRUNCATE TABLE usersTable");
     }
 
     private void executionSql(String SQL) {
-        Connection conn = null;
-        Statement  stmt = null;
 
-        try {
-            conn = Util.connection();
+        try (Connection conn = Util.connection()) {
             conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             conn.setAutoCommit(false);
 
-            stmt = conn.createStatement();
-            stmt.executeUpdate(SQL);
+            try (Statement stmt = conn.createStatement()) {
+                stmt.executeUpdate(SQL);
 
-            conn.commit();
-        } catch (SQLException e) {
-            rollback(conn);
-            e.printStackTrace();
-        } finally {
-            close(stmt);
-            close(conn);
-        }
-    }
-
-    private void close(AutoCloseable res) {
-        if (res != null) {
-            try {
-                res.close();
-            } catch (Exception ignored) {}
-        }
-    }
-
-    private void rollback(Connection conn) {
-        if (conn != null) {
-            try {
+                conn.commit();
+            } catch (SQLException e) {
                 conn.rollback();
-            } catch (SQLException ignor) {}
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
